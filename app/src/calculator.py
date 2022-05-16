c45e4240e24998d19aab13b54255556e1e1f7380
jiejiewang <jiejiewangzju@gmail.com>
2022-05-15 22:32:46 -0700

    add new files

---
+++ app/src/calculator.py
@@ -0,0 +1,10 @@
+class Calculator():
+    def add(self, a, b):
+        return a + b
+    def substract(self, a, b):
+        return a - b
+    def multiply(self, a, b):
+        return a * b
+    def divide(self, a, b):
+        return a / b
+
